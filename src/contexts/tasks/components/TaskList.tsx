57d8f0e2fd9246172f3aa1334b1a94b815033975
[dyad] <luizotavioltdm@gmail.com>
2026-06-12 14:43:56 -0300

    [dyad] Corrigindo import faltando Button no TaskList - wrote 1 file(s)

--- src/contexts/tasks/components/TaskList.tsx
+++ src/contexts/tasks/components/TaskList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CheckSquare, Clock, Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { useTasks } from "../hooks/useTasks";
